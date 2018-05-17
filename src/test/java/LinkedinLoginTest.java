5f8156ebd84d284fb0cf887fbb45fc14759390d8
ozua <oleg.zhuk@outlook.com>
2018-05-17 22:01:01 +0300

    lesson 6

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -20,40 +20,37 @@ public class LinkedinLoginTest {
 
     @Test
     public void successfulLoginTest() throws InterruptedException {
-        String actualLoginPageTitle = webDriver.getTitle();
+        LinkedInLoginPage linkedInLoginPage = new LinkedInLoginPage(webDriver);
 
-        Assert.assertEquals(webDriver.getTitle(), "LinkedIn: Log In or Sign Up", "Login page title is wrong");
+        Assert.assertEquals(linkedInLoginPage.getCurrentTitle(), "LinkedIn: Log In or Sign Up", "Login page title is wrong");
+        Assert.assertTrue(linkedInLoginPage.isSignInButtonDisplayed(), "Sign In button is not displayed");
 
-        LinkedInLoginPage linkedInLoginPage = new LinkedInLoginPage();
         linkedInLoginPage.login("erik.mouwes@gmail.com", "vlttar54");
 
-        Assert.assertTrue(submitButton.isDisplayed(), "Sign In button is not displayed");
+        LinkedInHomePage linkedInHomePage = new LinkedInHomePage(webDriver);
 
-        Assert.assertEquals(webDriver.getTitle(), "LinkedIn", "Homepage is not LinkedIn.com");
-        String actualHomePageTitle = webDriver.getTitle();
-        Assert.assertNotEquals(actualLoginPageTitle, actualHomePageTitle, "Page title did not change after Sign In");
-        Assert.assertEquals(webDriver.getCurrentUrl(), "https://www.linkedin.com/feed/", "Homepage url is wrong.");
+        Assert.assertTrue(linkedInHomePage.getCurrentTitle().contains("LinkedIn"), "Page title did not change after Sign In");
+        Assert.assertEquals(linkedInHomePage.getCurrentUrl(), "https://www.linkedin.com/feed/", "Homepage url is wrong.");
     }
 
     @Test
     public void negativeLoginTest() throws InterruptedException {
-        String actualLoginPageTitle = webDriver.getTitle();
+        LinkedInLoginPage linkedInLoginPage = new LinkedInLoginPage(webDriver);
 
-        Assert.assertEquals(actualLoginPageTitle, "LinkedIn: Log In or Sign Up", "Login page is wrong");
+        Assert.assertEquals(linkedInLoginPage.getCurrentTitle(), "LinkedIn: Log In or Sign Up", "Login page title is wrong");
+        Assert.assertTrue(linkedInLoginPage.isSignInButtonDisplayed(), "Sign In button is not displayed");
 
-        LinkedInLoginPage linkedInLoginPage = new LinkedInLoginPage();
         linkedInLoginPage.login("erik.mouwes@gmail.com", "4");
 
         sleep(3000);
 
-        String currentPageUrl = webDriver.getCurrentUrl();
-        String currentPageTitle = webDriver.getTitle();
+        String currentPageUrl = linkedInLoginPage.getCurrentUrl();
+        String currentPageTitle = linkedInLoginPage.getCurrentTitle();
 
         Assert.assertEquals(currentPageUrl, "https://www.linkedin.com/uas/login-submit", "Login-submit page is wrong");
         Assert.assertEquals(currentPageTitle, "Войти в LinkedIn", "Login-Submit page title is wrong");
 
         WebElement errorMessage = webDriver.findElement(By.xpath("//div[@role='alert']"));
-
         Assert.assertEquals(errorMessage.getText(), "При заполнении формы были допущены ошибки. Проверьте и исправьте отмеченные поля.", "Wrong error message text displayed");
     }
 
