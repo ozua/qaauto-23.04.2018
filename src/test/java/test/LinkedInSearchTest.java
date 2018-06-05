24f3da66ea6c20182266714a6e802cd400ae8c31
ozua <oleg.zhuk@outlook.com>
2018-06-05 18:01:18 +0300

    Home Work #11

--- src/test/java/test/LinkedInSearchTest.java
+++ src/test/java/test/LinkedInSearchTest.java
@@ -3,7 +3,7 @@ package test;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import page.LinkedInHomePage;
-import page.LinkedInSearchResults;
+import page.LinkedInSearchResultsPage;
 
 import java.util.List;
 
@@ -11,13 +11,15 @@ public class LinkedInSearchTest extends LinkedInBaseTest {
 
     @Test
     public void basicSearchTest() {
+        String searchTerm = "HR";
+
         LinkedInHomePage linkedInHomePage = linkedInLoginPage.login("erik.mouwes@gmail.com", "vlttar54");
         Assert.assertTrue(linkedInLoginPage.isPageLoaded(), "Login page is not loaded");
 
-        LinkedInSearchResults linkedInSearchResults = linkedInHomePage.search(searchTerm);
-        Assert.assertTrue(linkedInSearchResults.isPageLoaded(), "Search results page is not loaded");
+        LinkedInSearchResultsPage linkedInSearchResultsPage = linkedInHomePage.search(searchTerm);
+        Assert.assertTrue(linkedInSearchResultsPage.isPageLoaded(), "Search results page is not loaded");
 
-        List<String> searchResultList = linkedInSearchResults.getSearchResults();
+        List<String> searchResultList = linkedInSearchResultsPage.getSearchResultsCount()
         Assert.assertEquals(searchResultList.size(), 10, "Count of search results is wrong");
 
         for (String searchResult:searchResultList){
